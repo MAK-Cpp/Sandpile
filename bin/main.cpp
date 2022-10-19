4d349e6e29661c79924a5cffe17f2938182229e5
Maksim <spartmenik@gmail.com>
2022-10-19 19:58:37 +0300

    created structure of project

---
+++ bin/main.cpp
@@ -0,0 +1,8 @@
+// #include "bmppicture.h"
+#include "sandpile.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "checking c++.exe\n";
+    return 0;
+}
---
+++ lib/bmppicture.h
@@ -0,0 +1,3 @@
+struct BmpPicture {
+    int a;
+};
